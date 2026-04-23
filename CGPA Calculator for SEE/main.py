bcab8ae671900d9e7743e09a10e0c2e035e9f49f
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-04-24 00:13:54 +0530

    Add files via upload

---
+++ CGPA Calculator for SEE/main.py
@@ -0,0 +1,29 @@
+grading = {
+    "S" : 10,
+    "A" : 9,
+    "B" : 8,
+    "C" : 7,
+    "D" : 6,
+    "E" : 5,
+}
+numerator = 0
+credits = 0
+print('''🅲 🅶🅿 🅰   🅲🅰 🅻 🅲 🆄 🅻🅰 🆃🅾 🆁''')
+print("")
+print("Welcome to CGPA Calculator Brought to you by Darshan ❤")
+print("")
+count = int(input("Total number of Subjects appearing Including Labs:- "))
+i = 1
+while i <= count:
+    name = input(f"{i}.Subject Name - ")
+    credit = float(input(f"How many Credits does {name} Hold? - "))
+    grade = input("What is your expected Grade? ")
+    numerator += credit * grading[grade.upper()]
+    credits += credit
+    print("")
+    i += 1
+cgpa = (numerator / credits).__round__(2)
+print(f"Your CGPA according to your Expected Grades is {cgpa}.")
+print('Thank you❤️')
+
+
